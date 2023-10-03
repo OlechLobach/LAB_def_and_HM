a23c7576fa2cb4aa99ed59b74b9f89f97247874e
student <student@PC-C-09>
2023-10-03 20:24:29 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-while True:
+def display_quote(author, quote):
+    formatted_text = f'"{quote}"\n{author}'
+    print(formatted_text)
 
-    def is_happy_number(number):
-        num_str = str(number)
-        if len(num_str) != 6:
-            return False
-        first_half = num_str[:3]
-        second_half = num_str[3:]
-        sum_first_half = sum(map(int, first_half))
-        sum_second_half = sum(map(int, second_half))
-        return sum_first_half == sum_second_half
-    num = int(input('введіть число'))
-    result = is_happy_number(num)
-    if result:
-        print(f"{num} - щасливе число.")
-    else:
-        print(f"{num} - не щасливе число.")
-        продовжити = input("чи бажаєте ввести інше число?(так/ні):")
-        if продовжити.lower() !='так':
-            break
+quote = "Don't compare yourself with anyone in this world…\nif you do so, you are insulting yourself."
 
+author = "Bill Gates"
 
-
-
+display_quote(author, quote)
