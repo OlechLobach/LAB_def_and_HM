5a3b2b297c6efa5cb3dd66f872addc327f0c91a4
student <student@PC-C-09>
2023-10-03 20:46:51 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_product(start, end):
-    if start > end:
-        start, end = end, start
-    product = 1
-    for num in range(start, end + 1):
-        product *= num
-    return product
-start_num = int(input("введіть початкове число"))
-end_num = int(input("введіть кінцеве число"))
-result = calculate_product(start_num, end_num)
-print(f"Добуток чисел від {start_num} до {end_num} дорівнює {result}")
+def count_digits(number):
+    num_str = str(number)
+    count = len(num_str)
+    return count
+num = int(input("введіть число"))
+digit_count = count_digits(num)
+print(f"Кількість цифр у числі {num} дорівнює {digit_count}")
+
