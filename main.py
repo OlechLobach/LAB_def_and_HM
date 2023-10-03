99d33970e33f57c5297b68d48cb85962fb06049e
student <student@PC-C-09>
2023-10-03 19:14:07 +0300

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+def display_quote(author, quote):
+    print(f'"{quote}"\n{author}')
+
+quote = "Don't let the noise of others' opinions\ndrown out your own inner voice."
+author = "Steve Jobs"
+
+display_quote(author, quote)
