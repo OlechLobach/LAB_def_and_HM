25a020a692abebd9a670d02710e67174106738b2
student <student@PC-C-09>
2023-10-03 19:50:51 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-while True:
-    def display_even_numbers():
-        try:
-            start = int(input("Введіть початкове число: "))
-            end = int(input("Введіть кінцеве число: "))
-
-            print(f"Парні числа між {start} і {end}:")
-            for num in range(start, end + 1):
-                if num % 2 != 0:
-                    print(num)
-        except ValueError:
-            print("Будь ласка, введіть правильні числа.")
-
-    display_even_numbers()
-    продовжити = input('чи бажаєте ввести інші числа? (так/ні)')
-    if продовжити.lower() !='так':
-        break
+def display_line(length, direction, symbol):
+    if direction == "horizontal":
+        print(symbol * length)
+    elif direction == "vertical":
+        for _ in range(length):
+            print(symbol)
+    else:
+        print("Неправильний напрямок. Виберіть 'horizontal' або 'vertical'.")
+length_h = 5
+direction_h = "horizontal"
+symbol_h = "_"
+display_line(length_h, direction_h, symbol_h)
+length_v = 3
+direction_v = "vertical"
+symbol_v = "|"
+display_line(length_v, direction_v, symbol_v)
