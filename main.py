de23b2dd84cccb47f3900cc30a1b44bc68c41d96
student <student@PC-C-09>
2023-10-03 19:47:23 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-def display_quote(author, quote):
-    print(f'"{quote}"\n{author}')
+while True:
+    def display_even_numbers():
+        try:
+            start = int(input("Введіть початкове число: "))
+            end = int(input("Введіть кінцеве число: "))
 
-quote = "Don't let the noise of others' opinions\ndrown out your own inner voice."
-author = "Steve Jobs"
+            print(f"Парні числа між {start} і {end}:")
+            for num in range(start, end + 1):
+                if num % 2 != 0:
+                    print(num)
+        except ValueError:
+            print("Будь ласка, введіть правильні числа.")
 
-display_quote(author, quote)
+    display_even_numbers()
+    продовжити = input('чи бажаєте ввести інші числа? (так/ні)')
+    if продовжити.lower() !='так':
+        break
